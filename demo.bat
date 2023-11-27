f239a69a2a175eb4fa3b942c983f4d2ffce57786
Tobias Dirchsen Engbo <toen@itu.dk>
2023-11-27 13:22:26 +0100

    Mørk, stormfuld nat

--- demo.bat
+++ demo.bat
@@ -5,5 +5,6 @@ REM start go run Server/Server.go -port 1003 -replicas "1000,1001,1002"
 REM start go run Server/Server.go -port 1004 -replicas "1000,1001,1002,1003"
 
 start go run Client/Client.go -server 1000 -id "BidGod"
+start go run Client/Client.go -server 1001 -id "SortePowerRanger"
 start go run Client/Client.go -server 1002 -id "WantToBuySpaces"
 REM start go run Client/Client.go -server 1001 -id "TheAuctionator"
